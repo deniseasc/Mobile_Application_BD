9ef71cc2bf7815c29e441b31b65eb91251b47c2d
deniseasc <denise1526alves@gmail.com>
2019-05-29 05:53:25 +0900

    Tela de listar e meio design e banco

--- www/components/banco.js
+++ www/components/banco.js
@@ -1,5 +1,6 @@
 // This is a JavaScript file
 
+// Cadastrar
 $(document).on("click","#cadastrar",function(){
     var parametros = {
       "nome": $("#nome").val(),
@@ -23,3 +24,52 @@ $(document).on("click","#cadastrar",function(){
       }
     });
 });
+// Cadastrar
+
+$(document).on("click","#listar",function(){
+  $(location).attr("#href","#page2.html")
+});
+
+// Listar
+function listarPessoas(){
+  $.ajax({
+      type:"post", //como enviar
+      url:"https://web-service-bd-deniseasc.c9users.io/listar.php", //para onde enviar
+      dataType:"json",
+      //se der certo
+      success: function(data){
+        var itemLista = "";
+        $each(data.pessoas,function(i,dados){
+          itemLista += "<option value='"+dados.codigo+"'>"+dados.nome+"</option>";
+        });
+        $("#lista").html(itemLista);
+  },
+  // se der errado
+  error: function(data){
+    navigator.notification.alert(data);
+  }
+  });
+}
+// Listar
+
+// Listar um
+$(document).on("change","#lista",function(){
+  var codigoescolhido = $("option:selected",("#listar")).val();
+  $.ajax({
+      type:"get", //como enviar
+      url:"https://web-service-bd-deniseasc.c9users.io/listar-um.php", //para onde enviar
+      data:"id="+codigoescolhido,
+      dataType:"json",
+      //se der certo
+      success: function(data){
+        $("#cod").val(data.pessoa.cod);
+        $("#nome").val(data.pessoa.nome);
+        $("#email").val(data.pessoa.email);
+  },
+  // se der errado
+  error: function(data){
+    navigator.notification.alert(data);
+  }
+  });
+});
+// Listar um
